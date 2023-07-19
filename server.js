ee723a376a5abf6ad8dba527819eb051f6c9138a
AlphaGuilty <53876388+AlphaGuilty@users.noreply.github.com>
2023-07-19 06:36:28 -0400

    Version Alpha

--- server.js
+++ server.js
@@ -68,18 +68,19 @@ app.get('/model', (req, res) => {
 });
 
 app.post('/upload/:id', upload.single('image'), async (req, res) => {
+  try {
     const id = req.params.id;
     const image = req.file.filename;
     const name = req.body.name;
     const description = req.body.description;
-  
+
     const imagePath = path.join(__dirname, 'public/images', image);
     const metadata = await sharp(imagePath).metadata();
     const width = metadata.width;
     const height = metadata.height;
-  
+
     let newWidth, newHeight;
-  
+
     if (width > height) {
       newWidth = 10;
       newHeight = newWidth * height / width;
@@ -87,17 +88,22 @@ app.post('/upload/:id', upload.single('image'), async (req, res) => {
       newHeight = 10;
       newWidth = newHeight * width / height;
     }
-  
+
     const sql = `UPDATE pictures SET imagen = ?, nombre = ?, descripcion = ?, ancho = ?, alto = ? WHERE id = ?`;
     const values = [image, name, description, newWidth, newHeight, id];
-  
+
     db.query(sql, values, (err, result) => {
-      if (err) return res.json(err);
+      if (err) throw err;
       return res.json({ success: true });
     });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: err });
+  }
 });
 
 app.post('/uploadModel/:id', modelUpload.fields([{ name: 'model', maxCount: 1 }, { name: 'texture', maxCount: 1 }]), (req, res) => {
+  try {
     const id = req.params.id;
     const model = req.files['model'][0].filename;
     const texture = req.files['texture'][0].filename;
@@ -106,15 +112,18 @@ app.post('/uploadModel/:id', modelUpload.fields([{ name: 'model', maxCount: 1 },
     const size = req.body.size || 100;
     const tall = req.body.tall;
     const rotation = req.body.rotation;
-  
+
     const sql = `UPDATE models SET model3D = ?, textura = ?, nombre = ?, descripcion = ?, size = ?, altura = ?, rotacion = ? WHERE id = ?`;
     const values = [model, texture, name, description, size/100, tall, rotation * (Math.PI / 180), id];
-  
+
     db.query(sql, values, (err, result) => {
-        console.log(result)
-      if (err) return res.json(err);
+      if (err) throw err;
       return res.json({ success: true });
     });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: 'No se pudo enviar la solicitud' });
+  }
 });
 
 app.get('/pictures', (req, res) => {
